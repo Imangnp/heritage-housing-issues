c8ba6af883bc758febb6e0998b5d02f7991156a8
Imangnp <>
2023-07-02 19:22:44 +0000

    Add Project Summary page to Streamlit dashboard

--- app_pages/page_summary.py
+++ app_pages/page_summary.py
@@ -3,4 +3,51 @@ import streamlit as st
 
 def page_summary_body():
 
-    st.write("### Quick Project Summary")
+    st.write("### Quick Project Summary\n")
+
+    st.write(
+        f"The project is an ML app specifically developed for the"
+        f"visualization and prediction of house prices in Ames, Iowa.\n\n"
+        f"The app offers users the following functionalities:\n"
+        f"* **Correlation Analysis:** The application allows users to identify the"
+        f"correlation between house attributes and the sale price.\n"
+        f"* **Sale Price Prediction:** The application provides a predictive model"
+        f"that enables users to obtain accurate estimates for the sale price.\n"
+    )
+
+    st.info(
+        f"**Project Terms & Jargons**\n"
+        f" * **Variables**: Refers to the different attributes of a house, such as"
+        f" floor area, basement and garage.\n"
+        f" * **Target Variable**: The target variable in this study is the "
+        f"'SalePrice'. It represents the price at which a house was sold.\n\n"
+
+        f"**Project Dataset**\n"
+        f"* The dataset consists of properties that were sold in Ames, Iowa. "
+        f"It contains details about 24 distinct features for each property,"
+        f"which have the potential to impact the price. These features "
+        f"encompass various aspects of the houses, including floor area, "
+        f"basement, garage, kitchen and year built. We utilize this dataset "
+        f"to conduct our study on correlated features, train our machine "
+        f"learning model, and subsequently predict the sale prices of "
+        f"properties in the area.\n"
+        f"* The dataset encompasses houses constructed from 1872 to 2010, and "
+        f"it has been obtained from "
+        f"[Kaggle](https://www.kaggle.com/datasets/codeinstitute/housing-prices-data)"
+        f".\n\n"
+    )
+
+    # Link to README file, so the users can have access to full project documentation
+    st.write(
+        f"For additional information, please visit and **read** the "
+        f"**[Project's README file](https://github.com/Imangnp/heritage-housing-issues)**.")
+
+    # copied from README file - "Business Requirements" section
+    st.success(
+        f"** The project has 2 business requirements:** \n \n "
+        f"**1.** The client is interested in discovering how the house"
+        f"attributes correlate with the sale price. Therefore, the client expects data "
+        f"visualisations of the correlated variables against the sale price "
+        f"to show that. \n\n "
+        f"**2.** The client is interested in predicting the house sale price "
+        f"from her four inherited houses and any other house in Ames, Iowa. ")
