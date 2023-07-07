25faf912bc3ee50593f5253484446cf9ddfd9efe
Imangnp <>
2023-07-07 17:48:59 +0000

    Added 'ml_predict' page to the Streamlit dashboard

--- app_pages/page_ml_predict.py
+++ app_pages/page_ml_predict.py
@@ -1,6 +1,89 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+from src.data_management import load_house_prices_data, load_pkl_file
+from src.machine_learning.regression_evaluation import (
+    regression_performance, regression_evaluation, regression_evaluation_plots
+)
 
 
 def page_ml_predict_body():
 
+    # load pipeline files
+    version = 'v1'
+    best_regressor_pipeline = load_pkl_file(
+        "outputs/ml_pipeline/predict_sale_price/" +
+        f"{version}/best_regressor_pipeline.pkl"
+    )
+    sale_price_importance = plt.imread(
+        "outputs/ml_pipeline/predict_sale_price/" +
+        f"{version}/features_importance.png"
+    )
+    X_train = pd.read_csv(
+        f"outputs/ml_pipeline/predict_sale_price/{version}/X_train.csv")
+    X_test = pd.read_csv(
+        f"outputs/ml_pipeline/predict_sale_price/{version}/X_test.csv")
+    y_train = pd.read_csv(
+        f"outputs/ml_pipeline/predict_sale_price/{version}/y_train.csv")
+    y_test = pd.read_csv(
+        f"outputs/ml_pipeline/predict_sale_price/{version}/y_test.csv")
+
     st.write("### ML: Predict Sale Price")
+    # display pipeline training summary conclusions
+    st.info(
+        f"* The ML pipeline was developed to accurately predict the sale price"
+        f"of houses. \n"
+        f"* We aimed to achieve an R2 score of at least **0.75** on both "
+        f"the train set and test set to ensure reliable predictions. \n"
+        f"* We evaluated four models and found that the "
+        f"**RandomForestRegressor** demonstrated consistent performance."
+        f" It achieved an **R2 score** of **0.9** on the train set and "
+        f"**0.816** on the test set. \n"
+        f"* Based on these results, we refined the pipeline and trained it on "
+        f"the best features, ensuring accurate and reliable predictions of "
+        f"house sale prices."
+    )
+    st.write("---")
+
+    # show pipelines
+    st.write("* **ML pipeline to predict house sale price**")
+    st.write(best_regressor_pipeline)
+    st.write("---")
+
+    # show best features
+    st.write("* **The features the model was trained and their importance:**")
+    st.write(X_train.columns.to_list())
+    st.image(sale_price_importance)
+    st.write("---")
+
+    # evaluate performance on train and test set
+    st.write("## Pipeline Performance")
+    regression_performance(X_train=X_train, y_train=y_train,
+                           X_test=X_test, y_test=y_test,
+                           pipeline=best_regressor_pipeline)
+    st.write("---")
+
+    # Plot predicted versus actual sale price for train and test sets
+    st.write("**Regression Evaluation plots:**")
+    st.write(
+        f"* Comparing Predicted and Actual Sale Prices on Train & Test Sets:"
+        f"\n \n(*Please note that the plots may take some time to load*)"
+    )
+    regression_evaluation_plots(X_train=X_train, y_train=y_train,
+                                X_test=X_test, y_test=y_test,
+                                pipeline=best_regressor_pipeline,
+                                alpha_scatter=0.5)
+
+    st.write(
+        f"* The blue dots represent the actual prices, while the horizontal"
+        f" position of each dot represents the corresponding predicted "
+        f"prices from the ML pipeline."
+        f"The red line represents perfect alignment, where the predicted"
+        f" and actual prices are equal. \n"
+        f"* Upon analyzing the plot, it is evident that the majority of "
+        f"the blue dots closely align with the red line. This indicates "
+        f"that the ML pipeline's predictions generally match the actual "
+        f"values, demonstrating a strong correlation between the predicted"
+        f" and actual sale prices. This suggests that the ML pipeline is"
+        f" performing well in accurately predicting the sale prices of houses."
+    )
---
+++ src/machine_learning/regression_evaluation.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+
+# Code copied from Modeling and Evaluation notebook,
+# Evaluate on Train and Test Sets section
+
+
+# Evaluate the performance of a regression model on train and test sets
+def regression_performance(X_train, y_train, X_test, y_test, pipeline):
+    st.write("**Model Evaluation**")
+    st.write("* **Train Set**")
+    regression_evaluation(X_train, y_train, pipeline)
+    st.write("* **Test Set**")
+    regression_evaluation(X_test, y_test, pipeline)
+
+
+def regression_evaluation(X, y, pipeline):
+    prediction = pipeline.predict(X)
+    st.write(
+        'R2 Score:', r2_score(y, prediction).round(3))
+    st.write(
+        'Mean Absolute Error:', mean_absolute_error(y, prediction).round(3))
+    st.write(
+        'Mean Squared Error:', mean_squared_error(y, prediction).round(3))
+    st.write(
+        'Root Mean Squared Error:', np.sqrt(
+            mean_squared_error(y, prediction)).round(3))
+    st.write("\n")
+
+
+# Plot evaluation of regression model
+def regression_evaluation_plots(X_train, y_train, X_test, y_test, pipeline,
+                                alpha_scatter=0.5):
+
+    pred_train = pipeline.predict(X_train)
+    pred_test = pipeline.predict(X_test)
+
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 6))
+    sns.scatterplot(x=y_train.values.ravel(), y=pred_train,
+                    alpha=alpha_scatter, ax=axes[0])
+    sns.lineplot(x=y_train.values.ravel(),
+                 y=y_train.values.ravel(), color='red', ax=axes[0])
+    axes[0].set_xlabel("Actual")
+    axes[0].set_ylabel("Predictions")
+    axes[0].set_title("Train Set")
+
+    sns.scatterplot(x=y_test.values.ravel(), y=pred_test,
+                    alpha=alpha_scatter, ax=axes[1])
+    sns.lineplot(x=y_test.values.ravel(),
+                 y=y_test.values.ravel(), color='red', ax=axes[1])
+    axes[1].set_xlabel("Actual")
+    axes[1].set_ylabel("Predictions")
+    axes[1].set_title("Test Set")
+
+    st.pyplot(fig)
